3cf016573a50426909ce1b62de8d7ec7ccd65c76
mblai35 <mbstrong35@gmail.com>
2017-06-05 21:00:22 -0600

    small bn simulation

--- tinySimBN.R
+++ tinySimBN.R
@@ -7,35 +7,122 @@
 library(bnlearn)
 #-----------------------------------------------------------------------
 
-### Discrete network:
+### Discrete network x and y:
+  
+  # Create parent node x.
+  x <- as.factor(rep(c(1, 2, 3), each = 50))
+  
+  # Create child node y. 
+  y <- as.factor(c(sample(1:3, 50, replace = T, prob = c(.7, .2, .1)), 
+                   sample(2:4, 50, replace = T, prob = c(.4, .5, .1)), 
+                   sample(7:9, 50, replace = T, prob = c(.3, .3, .4))))
+  
+  # Combine into data frame. 
+  net <- data.frame(x, y)
+  
+  # Use a tabu search to learn the network. 
+  bn <- tabu(net, score = "bde", iss = 10, tabu = 50)
+  
+  # Plot the network. 
+  plot(bn)
+  
+  # Fit the parameters conditional on its structure. 
+  fit <- bn.fit(bn, net, method = "bayes")
+  
+  # Plot barchart of P(y|x).
+  bn.fit.barchart(fit$y)
+  
+  # List probabilities associated with barchart. 
+  fit$y
 
-# Create parent node x.
-x <- as.factor(rep(c(1, 2, 3), each = 50))
+### Discrete network x, y, and z:
 
-# Create child node y. 
-y <- as.factor(c(sample(1:3, 50, replace = T, prob = c(.7, .2, .1)), 
-                 sample(2:4, 50, replace = T, prob = c(.4, .5, .1)), 
-                 sample(7:9, 50, replace = T, prob = c(.3, .3, .4))))
+  # Create parent node x.
+  x <- as.factor(rep(c(1, 2, 3), each = 50))
+  
+  # Create child node y. 
+  y <- as.factor(c(sample(1:3, 50, replace = T, prob = c(.7, .2, .1)), 
+                   sample(2:4, 50, replace = T, prob = c(.4, .5, .1)), 
+                   sample(7:9, 50, replace = T, prob = c(.3, .3, .4))))
+  
+  # Create child node z. 
+  z <- as.factor(c(sample(3:5, 100, replace = T, prob = c(.2, .6, .2)), 
+                   sample(6:8, 50, replace = T, prob = c(.5, .1, .4))))
+  
+  
+  # Combine into data frame. 
+  net <- data.frame(x, y, z)
+  
+  # Use a tabu search to learn the network. 
+  bn <- tabu(net, score = "bde", iss = 10, tabu = 50)
+  
+  # Plot the network. 
+  plot(bn)
+  
+  # Fit the parameters conditional on its structure. 
+  fit <- bn.fit(bn, net, method = "bayes")
+  
+  # Plot barchart of P(y|x).
+  bn.fit.barchart(fit$y)
+  
+  # List probabilities associated with barchart. 
+  fit$y
+  
+  # Plot barchart of P(z|x).
+  bn.fit.barchart(fit$z)
+  
+  # List probabilities associated with barchart. 
+  fit$z
+  
+# Re-run with iss > 10. 
+  # Use a tabu search to learn the network. 
+  bn <- tabu(net, score = "bde", iss = 15, tabu = 50)
+  
+  # Plot the network. 
+  plot(bn)
+  
+  # Fit the parameters conditional on its structure. 
+  fit <- bn.fit(bn, net, method = "bayes")
+  
+  # List probabilities associated with barchart. 
+  fit$z # Notice the probabilities get more flat.
 
-# Combine into data frame. 
-net <- data.frame(x, y)
-
-# Use a tabu search to learn the network. 
-bn <- tabu(net, score = "bde", iss = 10, tabu = 50)
-
-# Plot the network. 
-plot(bn)
-
-# Fit the parameters conditional on its structure. 
-fit <- bn.fit(bn, net, method = "bayes")
-
-# Plot barchart of P(y|x).
-bn.fit.barchart(fit$y)
-
-# List probabilities associated with barchart. 
-fit$y
-
-
-
-
+### Discrete network w, x, and y:
+  
+  # Create parent node x.
+  w <- as.factor(rep(c(0, 1), times = c(100, 50)))
+  
+  # Create parent and child node x.
+  x <- as.factor(rep(c(1, 2, 3), each = 50))
+  
+  # Create child node y. 
+  y <- as.factor(c(sample(1:3, 50, replace = T, prob = c(.7, .2, .1)), 
+                   sample(2:4, 50, replace = T, prob = c(.4, .5, .1)), 
+                   sample(7:9, 50, replace = T, prob = c(.3, .3, .4))))
+  
+  # Combine into data frame. 
+  net <- data.frame(w, x, y)
+  
+  # Use a tabu search to learn the network. 
+  bn <- tabu(net, score = "bde", iss = 5, tabu = 50)
+  
+  # Plot the network. 
+  plot(bn)
+  
+  # Fit the parameters conditional on its structure. 
+  fit <- bn.fit(bn, net, method = "bayes")
+  
+  # Plot barchart of P(y|x).
+  bn.fit.barchart(fit$y)
+  
+  # List probabilities associated with barchart. 
+  fit$y
+  
+  # Plot barchart of P(x|w).
+  bn.fit.barchart(fit$x)
+  
+  # List probabilities associated with barchart. 
+  fit$x
+  
+  
 
