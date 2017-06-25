25e60d2114be07cd0b96f35f3724ab92aa015c76
mblai35 <mbstrong35@gmail.com>
2017-06-24 19:45:03 -0600

    Update mod.h

--- mod.h
+++ mod.h
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 #SBATCH --account=plantanalytics
-#SBATCH --time=16:00:00
+#SBATCH --time=168:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
+#SBATCH --mem=0
+#SBATCH --exclusive
+#SBATCH --constraint=hugemem
+#SBATCH --partition=hugemem
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=mstrong3@uwyo.edu
 #SBATCH --output=mod.out
